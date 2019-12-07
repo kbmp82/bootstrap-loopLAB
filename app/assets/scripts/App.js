d766f2647d12d252313681ed3ec0a71b1742adc8
Kevin <kbmp82@gmail.com>
2019-12-06 19:24:05 -0700

    Finished footer, modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,27 @@
 //jshint esversion:6
 import '../styles/styles.css';
 import 'lazysizes';
+import SmoothScroll from './modules/SmoothScroll';
+
+
 
 if(module.hot){
     module.hot.accept();
 }
+
+new SmoothScroll();
+/* contact modal */
+//create empty modal var to inject modal upon click
+// let modal;
+// document.querySelector('.open-modal').addEventListener('click', (e) => {
+// e.preventDefault();
+// if(typeof modal === 'undefined'){
+
+//     import( /* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+//         modal = new x.default();
+//         setTimeout(() => modal.modalOpen(), 20);
+//     }).catch(() => console.log('error'));
+// }else{
+
+// }
+// });
---
+++ app/assets/scripts/modules/SmoothScroll.js
@@ -0,0 +1,24 @@
+class SmoothScroll {
+    constructor() {
+        this.events();
+    }
+
+    events() {
+        $('#main-nav a').on('click', function (event) {
+            
+            if(this.hash !== "") {
+                event.preventDefault();
+
+                const hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function (){
+                    window.location.hash = hash;
+                });
+            }
+        })
+    }
+}
+
+
+export {SmoothScroll as default}
